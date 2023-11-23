75336262f1f40d91eeb60077d27c67c4f1a489a3
haeunyy <ibbihe@gmail.com>
2023-11-23 23:06:38 +0900

    first commit

---
+++ exam._class.py
@@ -0,0 +1,27 @@
+class Car:
+    def __init__(self, tier, price):
+        self.tier = tier
+        self.price = price
+
+    def info(self):
+        pass
+
+class Bicycle(Car):
+    def __init__(self, tier, price, machine):
+        super().__init__(tier, price)
+        self.machine = machine
+
+    def info(self):
+        print("바퀴수 : {0} \n가격 : {1} \n구동계 : {2}".\
+              format(self.tier, self.price, self.machine))
+
+
+ben = Bicycle(2, 25000, "시마노") 
+
+
+ben.info()
+
+# print(bmw.price)
+# print(bmw.tier)
+
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # def info(self):
+    #     print("바퀴수 : {0} \n가격 : {1}".format(self.tier, self.price))
